d2328c3de6ce794654bd14a66523b8343b1f30cf
Antanas Ramanauskas <antanasramana@gmail.com>
2024-12-18 01:43:40 +0200

    - added multivariate forecasting

--- main.py
+++ main.py
@@ -26,7 +26,11 @@ selected_cities = st.sidebar.multiselect(
     default=data['City'].unique()
 )
 
+st.sidebar.header("Forecast settings")
 forecast_horizon = st.sidebar.slider("Forecast Horizon (days)", min_value=7, max_value=90, value=30)
+required_pollutants = ['pm10', 'pm25', 'no2', 'o3', 'so2', 'co']
+unique_values_excluding_pollutants = [specie for specie in data["Specie"].unique() if specie not in required_pollutants]
+selected_regressor = st.sidebar.selectbox("Select a regressor:", unique_values_excluding_pollutants)
 
 general_tab, humidity_and_temp_tab, air_quality_tab, api_tab, insights_tab, forecasting_tab = st.tabs(["General", "Humidity & Temperature", "Air quality", "API tab'as", "Insights", "Forecasting"])
 
@@ -46,4 +50,4 @@ with insights_tab:
     build_insights_tab(data, selected_cities, selected_city)
 
 with forecasting_tab:
-    build_forecasting_tab(data, selected_city, forecast_horizon)
+    build_forecasting_tab(data, selected_city, forecast_horizon, selected_regressor)
